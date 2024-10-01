d01f220f905d00eb5211055bef3b3fe8090e57df
Joe <jojoforjob@gmail.com>
2024-10-01 16:27:52 +0800

    測試刪除

--- 209 長度最小的子數組.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        min_len = float('inf')
-        r,l = 0,0
-        x_sum = 0
-        max_l = len(nums)
-        while r < max_l:
-            x_sum += nums[r]
-        return min_len if min_len != float('inf') else 0
